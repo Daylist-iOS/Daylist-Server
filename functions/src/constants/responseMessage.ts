ceb995c262978b7d193c26515d7f4dcf089f4d56
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-13 01:16:43 +0900

    ➕ Add: Util 파일 추가

---
+++ functions/src/constants/responseMessage.ts
@@ -0,0 +1,23 @@
+module.exports = {
+    // 오류
+    NULL_VALUE: "필요한 값이 없습니다",
+    OUT_OF_VALUE: "요청한 값이 잘못되었습니다",
+    INTERNAL_SERVER_ERROR: "서버 내부 오류",
+    TOKEN_ERROR: "유효하지 않은 토큰입니다",
+    TOKEN_EXPIRED: "만료된 토큰입니다",
+    TOKEN_INVALID: "유효하지 않은 토큰입니다",
+    TOKEN_EMPTY: "토큰이 없습니다",
+    TOKEN_RENEWAL_SUCCESS: "토큰 갱신 완료",
+    INCORRECT_CATEGORY: "올바르지 않은 카테고리 아이디입니다.",
+    INCORRECT_SORT: "정렬값이 잘못되었습니다.",
+
+    // 회원가입
+    CREATED_USER: "회원 가입 성공",
+    DELETE_USER: "회원 탈퇴 성공",
+    ALREADY_EMAIL: "이미 사용중인 이메일입니다.",
+
+    // 로그인
+    LOGIN_SUCCESS: "로그인 성공",
+    LOGIN_FAIL: "로그인 실패",
+    NO_USER: "존재하지 않는 회원입니다.",
+};
